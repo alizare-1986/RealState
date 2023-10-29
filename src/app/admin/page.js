d8bc0d9bdc7d36cd60bbd71e6375a5dd2258b5e4
alizare-1986 <alianayatzare.a.a.1986@gmail.com>
2023-10-29 11:16:45 +0330

    the end

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -7,17 +7,20 @@ import DashboardSidebar from "@/layout/DashboardSidebar";
 import AdminPage from "@/template/AdminPage";
 import Profile from "@/models/Profile";
 
+export const metadata = {
+  title: "پنل ادمین",
+};
 async function Admin() {
   await connectDB();
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signin");
   const user = await User.findOne({ email: session.user.email });
   if (user.role !== "ADMIN") redirect("/dashboard");
-  const profiles =await Profile.find({published:false})
+  const profiles = await Profile.find({ published: false });
 
   return (
     <DashboardSidebar role={user.role} email={user.email}>
-      <AdminPage profiles={profiles}/>
+      <AdminPage profiles={profiles} />
     </DashboardSidebar>
   );
 }
--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -4,15 +4,21 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import connectDB from "@/utils/connectDB";
 import User from "@/models/User";
+export const metadata = {
+  title: "پنل کاربری",
+};
 
 async function DashboardLayout({ children }) {
-    const session = await getServerSession(authOptions)
-    if(!session) redirect("/signin")
-    await connectDB()
-  const user =await User.findOne({email:session.user.email})
- if(!user) return <h3>مشکلی پیش امده است</h3>
-  return <DashboardSidebar role={user.role}  email={user.email}>{children}</DashboardSidebar>;
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/signin");
+  await connectDB();
+  const user = await User.findOne({ email: session.user.email });
+  if (!user) return <h3>مشکلی پیش امده است</h3>;
+  return (
+    <DashboardSidebar role={user.role} email={user.email}>
+      {children}
+    </DashboardSidebar>
+  );
 }
 
 export default DashboardLayout;
-
