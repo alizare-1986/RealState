e6bff2d7f25293fe8e67870fd410cb74745aeda5
alizare-1986 <alianayatzare.a.a.1986@gmail.com>
2023-10-29 10:31:34 +0330

    debuging

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,20 +1,29 @@
+import Profile from "@/models/Profile";
 import BuyResidentialPage from "@/template/BuyResidentialPage";
-
+import connectDB from "@/utils/connectDB";
 
 async function BuyResidential({ searchParams }) {
-  const res = await fetch("http://localhost:3000/api/profile" );
-  const data = await res.json();
-  if (data.error) return <h3>مشکلی پیش آمده است</h3>;
-  let finalData = data.data;
-  if (searchParams.category) {
-    finalData = finalData.filter((i) => i.category === searchParams.category);
+  // const res = await fetch("http://localhost:3000/api/profile", {
+  //   cache: "no-store",
+  // });
+  // const data = await res.json();
+  // if (data.error) return <h3>مشکلی پیش آمده است</h3>;
+  // let finalData = data.data;
+  // if (searchParams.category) {
+  //   finalData = finalData.filter((i) => i.category === searchParams.category);
+  // }
+  await connectDB()
+  let profiles =await Profile.find({published:true})
+  if(searchParams.category){
+    profiles=profiles.filter((i)=>i.category===searchParams.category)
   }
 
   return (
     <div>
-      <BuyResidentialPage data={finalData} />
+      <BuyResidentialPage data={profiles} />
     </div>
   );
 }
 
 export default BuyResidential;
+
